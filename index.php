1549fee897da27ce28dc7a83986ebc9e0f1f2be6
Enrique Corona <enrique_cr1990@hotmail.com>
2020-06-08 22:06:39 -0500

    se Agrega el index de php

---
+++ index.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<html lang="">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge"/>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
+
+    <meta name="author" content="Enrique Corona Ricaño">
+    <meta name="description" content="Repositorio para las materias de la Maestría en Dirección e Ingenieria de Sitios Web de UNIR México">
+    <meta name="keywords" content="Enrique Corona Ricaño, Estudiante de Maestria en UNIR México, Maestría en Dirección e Ingenieria de Sitios Web ">
+
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css"
+          integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+
+    <title>Maestria UNIR - Enrique Corona</title>
+</head>
+<body>
+
+<div class="container">
+
+    <div class="row">
+        <div class="form-group col-lg-12 col-md-12">
+            <div class="alert alert-secondary">Maestría en Dirección e Ingeniería de Sitios Web</div>
+        </div>
+    </div>
+
+</div>
+
+<script src="https://code.jquery.com/jquery-3.5.1.min.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js"
+        integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1"
+        crossorigin="anonymous"></script>
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"
+        integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM"
+        crossorigin="anonymous"></script>
+
+</body>
+</html>
