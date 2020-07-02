97a52a6a9077f2c30d49d676daec19736c1e39b3
Enrique Corona <enrique_cr1990@hotmail.com>
2020-07-01 19:00:51 -0500

    segunda tarea de la materia de lenguajes y estandares, pagina html con css3

--- materias/1er_semestre/letw/entregables.php
+++ materias/1er_semestre/letw/entregables.php
@@ -12,4 +12,18 @@
             </div>
         </div>
     </div>
+
+    <div class="form-group col-xl-3 col-lg-4 col-md-6 col-sm-12">
+        <div class="card">
+            <div class="card-body">
+                <h5 class="card-title">Sitio Web HTML5 con CSS3</h5>
+                <p class="card-text text-justify">
+                    El trabajo de esta asignatura consistirá en la tomar el trabajo previo de HTML5 y aplicar hojas de
+                    estilo para entregar un sitio web formado por al menos 4 documentos HTML5 enlazados, y al menos una
+                    hoja de estilo externa enlazada. No se permiten elementos de computación.
+                </p>
+                <a href="sitio_web_css/index.html" target="_blank" class="btn btn-outline-primary"><i class="fa fa-eye"></i> Ver tarea</a>
+            </div>
+        </div>
+    </div>
 </div>
