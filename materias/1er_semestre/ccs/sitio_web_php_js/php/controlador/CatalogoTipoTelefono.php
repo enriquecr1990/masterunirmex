21adc63e2c9136fb91d9886a12ed8e32aef702ca
Enrique Corona <enrique_cr1990@hotmail.com>
2020-07-26 19:41:00 -0500

    se sube el php de database para comprobar el server de azure y checar los datos de configuracion para la BD

---
+++ materias/1er_semestre/ccs/sitio_web_php_js/php/controlador/CatalogoTipoTelefono.php
@@ -0,0 +1,6 @@
+<?php
+var_dump($_SERVER);exit;
+//se incluyen los archivos php necesarios para el controlador de catalogoTipo telefono
+include_once ('../modelo/CatalogoTipoTelefono.php');
+
+$catalogoTipoTelefono = new CatalogoTipoTelefono();
